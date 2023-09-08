ce53e23fe027e46e423d817ccf00f5f991a1d825
Daniel Boll <danielboll.academico@gmail.com>
2023-09-08 08:34:03 -0300

    chore: add .gitignore file to ignore /target directory chore: add Cargo.toml file to specify project metadata and dependencies chore: add test.rinha file to assets directory for testing purposes chore: add rustfmt.toml file to configure Rust code formatting feat: add interpreter module to src directory to implement the Rinha interpreter feat: implement Value enum and its Display trait implementation in interpreter module feat: implement AnonymousFunction and Env structs in interpreter module feat: implement Env methods in interpreter module feat: implement interpret_from_ast function in interpreter module to interpret Rinha AST feat: implement interpret function in interpreter module to interpret Rinha terms feat: implement interpret_binary function in interpreter module to interpret binary operations feat: add lib.rs file to src directory to define the interpreter module feat: add main.rs file to src directory to run the Rinha interpreter on test.rinha file

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use rinha::rinha;
+use rinha_de_compiler::interpreter;
+
+fn main() {
+  let mut errors = vec![];
+  let input = std::fs::read_to_string("assets/test.rinha").unwrap();
+  let ast = rinha::FileParser::new()
+    .parse(&mut errors, "assets/test.rinha", &input)
+    .unwrap();
+
+  interpreter::interpret_from_ast(ast.expression);
+}
